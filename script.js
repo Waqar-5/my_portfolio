f3c4c4f6a9a0eefb561d1ba69e6a2760b21308d6
Waqar-5 <wa5134810@gmail.com>
2025-12-08 13:00:50 +0500

    added Carousel in skills

--- script.js
+++ script.js
@@ -84,3 +84,55 @@ document.getElementById("contact-form").addEventListener("submit", async functio
     });
   }
 });
+
+
+
+
+
+
+// skills
+  const skills = [
+  { name: "HTML", img: "./assets/html.webp", level: 95 },
+  { name: "CSS / Bootstrap", img: "./assets/css.avif", level: 90 },
+  { name: "JavaScript", img: "./assets/JS.avif", level: 85 },
+  { name: "Next.js", img: "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg", level: 60 },
+  // { name: "React", img: "https://cdn-icons-png.flaticon.com/512/1183/1183672.png", level: 85 },
+  // { name: "Tailwind CSS", img: "https://cdn.worldvectorlogo.com/logos/tailwindcss-icon.svg", level: 50 },
+  { name: "Tailwind CSS", img: "https://tse2.mm.bing.net/th/id/OIP.pP2p8guRbZhxjPk0PFJnMAHaHa?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3", level: 50 },
+
+  { name: "Python", img: "https://cdn-icons-png.flaticon.com/512/5968/5968350.png", level: 80 },
+  { name: "OpenAI Agents SDK", img: "https://tse2.mm.bing.net/th/id/OIP.QQMzuDuw-Ed49NkYNr905QHaEW?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3", level: 60 },
+  // { name: "Node.js", img: "https://cdn-icons-png.flaticon.com/512/919/919825.png", level: 65 }
+];
+
+
+
+  const carouselContent = document.getElementById("carouselContent");
+
+  for(let i=0; i<skills.length; i+=3){
+    const slideSkills = skills.slice(i, i+3);
+    const slideDiv = document.createElement("div");
+    slideDiv.classList.add("carousel-item");
+    if(i === 0) slideDiv.classList.add("active");
+
+    let innerHTML = `<div class="row g-4 justify-content-center">`;
+    slideSkills.forEach(skill => {
+      innerHTML += `
+        <div class="col-md-3">
+          <div class="skill-card shadow" style="--skill-level: ${skill.level}%">
+            <img src="${skill.img}" alt="${skill.name}">
+            <div class="overlay">
+              ${skill.name}
+              <div class="progress">
+                <div class="progress-bar"></div>
+              </div>
+              <small>${skill.level}% proficiency</small>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+    innerHTML += `</div>`;
+    slideDiv.innerHTML = innerHTML;
+    carouselContent.appendChild(slideDiv);
+  }
